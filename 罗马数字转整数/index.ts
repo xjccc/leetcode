3dd4af914c5d2cd48e535dd581a22cfae1922be8
xjccc <546534045@qq.com>
2023-11-27 20:12:16 +0800

    update: romanToInt

--- 罗马数字转整数/index.ts
+++ 罗马数字转整数/index.ts
@@ -32,3 +32,20 @@ function romanToInt(s: string): number {
   }
   return result
 }
+
+const dict:Map<string, number> = new Map([["I", 1],["V", 5], ["X", 10], ["L", 50], ["C", 100], ["D", 500], ["M", 1000]]);
+
+function romanToInt1(s: string): number {
+    let sum = 0;
+    for(let i = 0; i < s.length; i++){
+        const char = s[i];
+        const value = dict.get(char);
+        if(i<s.length-1 && value < dict.get(s[i+1])){
+            sum -= value
+        }else{
+            sum += value
+        }
+    }
+
+    return sum;
+}
