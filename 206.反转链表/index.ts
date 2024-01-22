df6021ff8368ae2613303cd7d667adc62bcbb44b
xjccc <546534045@qq.com>
2024-01-22 13:40:03 +0800

    feat: reverseList

---
+++ 206.反转链表/index.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let cur = head
+  let prev = null
+  while (cur) {
+    const temp = cur.next
+    cur.next = prev
+    prev = cur
+    cur = temp
+  }
+  return prev
+}
